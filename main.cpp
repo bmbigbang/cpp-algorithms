259804cc387c83c9601bf4991d548b641c30e6bc
Ardavan Shafiee <bm.bigbang@gmail.com>
2018-06-10 21:32:20 +0100

    using arrays in functions for mergeSort

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "main.h"
 
 int main() {
-    int i = 8;
-    int cube = i*i*i;
-    do {
-        cout << cube << endl;
-        i++;
-        cube = i*i*i;
-    } while (cube <= 10000);
+    int size1 = 2;
+    int size2 = 4;
+    int list1[size1] = {45, 56};
+    int list2[size2] = {67, 88, 90, 1};
+
+    int size3 = size1 + size2;
+    int list3[size3];
+    mergeSort(list1, list2, list3, size1, size2);
+
+
+    for (int i = size1 + size2 - 1; i >= 0; i--) {
+        cout << list3[i] << endl;
+    }
 }
