df9de91576bbc21acca48c5204a91b741ad6ac98
Ardavan Shafiee <bm.bigbang@gmail.com>
2018-06-11 09:20:14 +0100

    2d object sorting using vectors and tuples and sort

--- main.cpp
+++ main.cpp
@@ -1,17 +1,20 @@
 #include "main.h"
 
 int main() {
-    int size1 = 2;
-    int size2 = 4;
-    int list1[size1] = {45, 56};
-    int list2[size2] = {67, 88, 90, 1};
+    int inputKey;
+    char inputVal[50];
+    string temp;
+    vector<tuple<string, int>> m;
+    for (int i = 0; i < 4; i++) {
+        cout << "Enter a Key-Value pair seperated by space:" << endl;
+        scanf("%d%s", &inputKey, inputVal);
+        temp = string(inputVal);
+        m.emplace_back(tuple<string, int>(temp, inputKey));
+    }
 
-    int size3 = size1 + size2;
-    int list3[size3];
-    mergeSort(list1, list2, list3, size1, size2);
+    sort(m.begin(), m.end(), compare);
 
-
-    for (int i = size1 + size2 - 1; i >= 0; i--) {
-        cout << list3[i] << endl;
+    for (tuple<string, int> elem : m) {
+        cout << get<0>(elem) << ' ';
     }
 }
--- main.h
+++ main.h
@@ -6,31 +6,15 @@
 #define UDACITY_COURSE_MAIN_H
 
 #include <iostream>
+#include <map>
+#include <algorithm>
 #include <cstdio>
 
 using namespace std;
 
-void mergeSort(int list1[], int list2[], int* list3, int size1, int size2) {
-    int size3 = size1 + size2;
-
-    for (int i = 0; i < size1; i++) {
-        list3[i] = list1[i];
-    }
-    for (int i = 0; i < size2; i++) {
-        list3[i + size1] = list2[i];
-    }
-
-    int temp;
-    for (int i = 0; i < size3 - 1; i++) {
-        for (int j = i; j < size3; j++) {
-            if (list3[i] > list3[j]) {
-                temp = list3[j];
-                list3[j] = list3[i];
-                list3[i] = temp;
-            }
-        }
-    }
-
+int compare(tuple<string, int>& a, tuple<string, int>& b)
+{
+    return ( get<1>(a) > get<1>(b) );
 }
 
 
