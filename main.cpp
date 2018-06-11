0cb51403ea10d414068a45005eec75dbf072cc62
Ardavan Shafiee <bm.bigbang@gmail.com>
2018-06-11 10:11:54 +0100

    palindrome detection using strings, vectors and scanf for inputs

--- main.cpp
+++ main.cpp
@@ -3,9 +3,28 @@
 int main() {
     int inputKey;
     char inputVal[50];
+    string s;
+    int sSize;
+    bool isPalindrome = true;
+    vector<string> p;
 
-    for (int i = 40; i < 127; i++) {
-        cout << hex << i << "-" << oct << i << "-" << (char) i << endl;
+    for (int i = 0; i < 4; i++) {
+        cout << "Enter a word:" << endl;
+        scanf("%s", inputVal);
+        s = string(inputVal);
+        sSize = s.length();
+        for (int j = 0; j < (sSize + 1 / 2); j++) {
+            if (s[j] != s[sSize - j - 1]) {
+                isPalindrome = false;
+            }
+        }
+
+        if (isPalindrome) {
+            p.push_back(s);
+        }
     }
 
+    for (auto i : p) {
+        cout << i << endl;
+    }
 }
