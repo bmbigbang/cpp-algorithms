2f2f0398e08489c19be5be828d7afc3f03889d5b
Ardavan Shafiee <bm.bigbang@gmail.com>
2018-06-11 11:13:13 +0100

    string search through .find built-in and scanf for character capturing

--- main.cpp
+++ main.cpp
@@ -1,31 +1,34 @@
 #include "main.h"
 
 int main() {
-    int inputKey;
     char inputVal[50];
     string s;
-    string t;
-    int sSize;
-    bool isPalindrome = true;
-    vector<string> p;
+    string item1 = "a";
+    string item2 = "is";
+    int score1, score2;
+    vector<int> p;
+    size_t found;
 
     for (int i = 0; i < 4; i++) {
+        score1 = -1;
+        score2 = -1;
         cout << "Enter a word:" << endl;
-        scanf("%s", inputVal);
+        scanf(" %49[0-9a-zA-Z ]", inputVal);
         s = string(inputVal);
-        t = s;
-        transform(t.begin(), t.end(), t.begin(), ::tolower);
 
-        sSize = s.length();
-        for (int j = 0; j < (sSize + 1 / 2); j++) {
-            if (t[j] != t[sSize - j - 1]) {
-                isPalindrome = false;
-            }
+        found = s.find(item1);
+        if (found != string::npos) {
+            cout << found << endl;
+            score1 = (int) found;
         }
 
-        if (isPalindrome) {
-            p.push_back(s);
+        found = s.find(item2);
+        if (found != string::npos) {
+            cout << found << endl;
+            score2 = (int) found;
         }
+
+        p.emplace_back(score1 + score2);
     }
 
     for (auto i : p) {
